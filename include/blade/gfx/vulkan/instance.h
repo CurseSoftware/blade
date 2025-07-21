ed342f93c9a30dc976f171dba00c82fdf3d96577
raustin9 <raustin9@vols.utk.edu>
2025-07-21 19:07:45 -0400

    begin builder pattern for vulkan types

---
+++ include/blade/gfx/vulkan/instance.h
@@ -0,0 +1,40 @@
+#ifndef BLADE_GFX_VULKAN_INTANCE$a
+#define BLADE_GFX_VULKAN_INTANCE$a
+
+#include "gfx/vulkan/common.h"
+#include <optional>
+
+//namespace blade
+//{
+//    namespace gfx
+//    {
+//        namespace vk
+//        {
+//            struct version
+//            {
+//                struct { u32 value = 0; } major;
+//                struct { u32 value = 0; } minor ;
+//                struct { u32 value = 0; } patch;
+//            };
+//
+//            struct instance
+//            {
+//                struct builder
+//                {
+//                    builder& set_application_name(const char* app_name) noexcept;
+//                    builder& set_engine_name(const char* engine_name) noexcept;
+//                    builder& enable_debug_messenger() noexcept;
+//                    builder& set_app_version(struct version v = version{.major{1}, .minor {0}, .patch {0}}) noexcept;
+//                    builder& set_engine_version(struct version v = version{.major{1}, .minor {0}, .patch {0}}) noexcept;
+//                    builder& enable_validation_layers() noexcept;
+//                    
+//                    std::optional<instance> build();
+//                };
+//
+//                void destroy();
+//            };
+//        } // vk namespace
+//    } // gfx namespace
+//} // blade namespace
+
+#endif // BLADE_GFX_VULKAN_INTANCE$a
