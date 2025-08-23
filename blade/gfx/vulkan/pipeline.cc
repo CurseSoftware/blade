f1a36addb42e1667df2593c47bdc3d12ab576c1e
raustin9 <raustin9@vols.utk.edu>
2025-08-23 17:17:08 -0400

    add builder methods for pipeline creation

--- blade/gfx/vulkan/pipeline.cc
+++ blade/gfx/vulkan/pipeline.cc
@@ -11,13 +11,8 @@ namespace blade
     {
         namespace vk
         {
-            std::optional<std::shared_ptr<pipeline>> pipeline::builder::build() const noexcept
+            std::optional<std::shared_ptr<pipeline>> pipeline::builder::build() noexcept
             {
-//                std::array<VkDynamicState, 2> dynamic_states = {
-//                    VK_DYNAMIC_STATE_VIEWPORT
-//                    , VK_DYNAMIC_STATE_SCISSOR
-//                };
-
                 // class pipeline pipeline { info.device };
                 auto pipeline = std::make_shared<class pipeline>(info.device);
 
@@ -43,70 +38,17 @@ namespace blade
                     .primitiveRestartEnable = VK_FALSE
                 };
 
-                VkPipelineViewportStateCreateInfo viewport_state {
-                    .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
-                    .viewportCount = static_cast<u32>(info.viewports.size()),
-                    .pViewports = info.viewports.data(),
-                    .scissorCount = static_cast<u32>(info.scissors.size()),
-                    .pScissors = info.scissors.data()
-                };
+                // Move this into add_viewport method and make build() a const method
+                info.viewport_info.viewportCount = static_cast<u32>(info.viewports.size());
+                info.viewport_info.pViewports = info.viewports.data();
+                info.viewport_info.scissorCount = static_cast<u32>(info.scissors.size());
+                info.viewport_info.pScissors = info.scissors.data();
 
-                VkPipelineRasterizationStateCreateInfo rasterizer {
-                    .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
-                    .depthClampEnable = VK_FALSE,
-                    .rasterizerDiscardEnable = VK_FALSE,
-                    .polygonMode = VK_POLYGON_MODE_FILL,
-                    .cullMode = VK_CULL_MODE_BACK_BIT,
-                    .frontFace = VK_FRONT_FACE_CLOCKWISE,
-                    .depthBiasEnable = VK_FALSE,
-                    .depthBiasConstantFactor = 0.0f,
-                    .depthBiasClamp = 0.0f,
-                    .depthBiasSlopeFactor = 0.0f,
-                    .lineWidth = 1.0f,
-                };
-
-                VkPipelineMultisampleStateCreateInfo multisampling {
-                    .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
-                    .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
-                    .sampleShadingEnable = VK_FALSE,
-                    .minSampleShading = 1.0f,
-                    .pSampleMask = nullptr,
-                    .alphaToCoverageEnable = VK_FALSE,
-                    .alphaToOneEnable = VK_FALSE,
-                };
-
-                VkPipelineColorBlendAttachmentState color_blend_attachment {
-                    .blendEnable = VK_FALSE,
-                    .srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
-                    .dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
-                    .colorBlendOp = VK_BLEND_OP_ADD,
-                    .srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE,
-                    .dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
-                    .alphaBlendOp = VK_BLEND_OP_ADD,
-                    .colorWriteMask = VK_COLOR_COMPONENT_R_BIT
-                                    | VK_COLOR_COMPONENT_G_BIT
-                                    | VK_COLOR_COMPONENT_B_BIT
-                                    | VK_COLOR_COMPONENT_A_BIT,
-                };
-
-                VkPipelineColorBlendStateCreateInfo color_blending {
-                    .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
-                    .logicOpEnable = VK_FALSE,
-                    .logicOp = VK_LOGIC_OP_COPY,
-                    .attachmentCount = 1,
-                    .pAttachments = &color_blend_attachment,
-                    .blendConstants = { 0.0f, 0.0f, 0.0f, 0.0f },
-                };
-
-                VkPipelineLayoutCreateInfo pipeline_layout_info {
-                    .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
-                    .setLayoutCount = 0,
-                    .pSetLayouts = nullptr,
-                    .pushConstantRangeCount = 0,
-                    .pPushConstantRanges = nullptr,
-                };
-
-                if (VK_SUCCESS != vkCreatePipelineLayout(info.device.lock()->handle(), &pipeline_layout_info, info.allocation_callbacks, &pipeline->_layout))
+                if (VK_SUCCESS != vkCreatePipelineLayout(
+                    info.device.lock()->handle(), 
+                    &info.pipeline_layout_info, 
+                    info.allocation_callbacks, 
+                    &pipeline->_layout))
                 {
                     return std::nullopt;
                 }
@@ -124,13 +66,13 @@ namespace blade
                             .sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
                             .stageCount = static_cast<u32>(info.shader_stages.size()),
                             .pStages = info.shader_stages.data(),
-                            .pVertexInputState = &vertex_input,
-                            .pInputAssemblyState = &input_assembly,
-                            .pViewportState = &viewport_state,
-                            .pRasterizationState = &rasterizer,
-                            .pMultisampleState = &multisampling,
+                            .pVertexInputState = &info.vertex_info,
+                            .pInputAssemblyState = &info.input_assembly_info,
+                            .pViewportState = &info.viewport_info,
+                            .pRasterizationState = &info.rasterization_info,
+                            .pMultisampleState = &info.multisampler_info,
                             .pDepthStencilState = nullptr,
-                            .pColorBlendState = &color_blending,
+                            .pColorBlendState = &info.color_blend_info,
                             .pDynamicState = &dynamic_state,
                             .layout = pipeline->_layout,
                             .renderPass = info.renderpass,
@@ -232,7 +174,192 @@ namespace blade
 
                 return *this;
             }
+
+            pipeline::builder& pipeline::builder::add_vertex_input_binding_description(const VkVertexInputBindingDescription binding_descriptions) noexcept
+            {
+                info.vertex_binding_descriptions.push_back(binding_descriptions);
+
+                return *this;
+            }
             
+            pipeline::builder& pipeline::builder::add_vertex_input_attribute_description(const VkVertexInputAttributeDescription attribute_descriptions) noexcept
+            {
+                info.vertex_attribute_descriptions.push_back(attribute_descriptions);
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_input_assembly_topology(const VkPrimitiveTopology topology) noexcept
+            {
+                info.input_assembly_info.topology = topology;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_input_assembly_primitive_restart(const VkBool32 restart) noexcept
+            {
+                info.input_assembly_info.primitiveRestartEnable = restart;
+                info.input_assembly_info.flags = 0;
+
+                return *this;
+            }
+
+            pipeline::builder& pipeline::builder::set_input_assembly_flags(const VkPipelineInputAssemblyStateCreateFlags flags) noexcept
+            {
+                info.input_assembly_info.flags |= flags;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_input_assembly_pnext(const void* p_next) noexcept
+            {
+                info.input_assembly_info.pNext = p_next;
+
+                return *this;
+            }
+
+            pipeline::builder& pipeline::builder::set_rasterization_pnext(const void* p_next) noexcept
+            {
+                info.rasterization_info.pNext = p_next;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_rasterization_depth_clamp(const VkBool32 clamp) noexcept
+            {
+                info.rasterization_info.depthClampEnable = clamp;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_rasterization_discard_enable(const VkBool32 discard_enable) noexcept
+            {
+                info.rasterization_info.rasterizerDiscardEnable = discard_enable;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_rasterization_cull_mode(const VkCullModeFlags cull_mode) noexcept
+            {
+                info.rasterization_info.cullMode = cull_mode;
+                
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_rasterization_front_face(const VkFrontFace front_face) noexcept
+            {
+                info.rasterization_info.frontFace = front_face;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_rasterization_depth_bias(const VkBool32 depth_bias) noexcept
+            {
+                info.rasterization_info.depthBiasEnable = depth_bias;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_rasterization_depth_bias_constant_factor(const f32 constant_factor) noexcept
+            {
+                info.rasterization_info.depthBiasConstantFactor = constant_factor;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_rasterization_depth_bias_clamp(const f32 bias_clamp) noexcept
+            {
+                info.rasterization_info.depthBiasClamp = bias_clamp;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_rasterization_depth_bias_slope_factor(const f32 slope_factor) noexcept
+            {
+                info.rasterization_info.depthBiasSlopeFactor = slope_factor;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_rasterization_line_width(const f32 line_width) noexcept
+            {
+                info.rasterization_info.lineWidth = line_width;
+
+                return *this;
+            }
+                            
+            pipeline::builder& pipeline::builder::set_multisampler_rasterization_samples(const VkSampleCountFlagBits samples) noexcept
+            {
+                info.multisampler_info.rasterizationSamples = samples;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::set_multisampler_sample_shading(const VkBool32 shading) noexcept
+            {
+                info.multisampler_info.sampleShadingEnable = shading;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::add_multisampler_sample_mask(const VkSampleMask* mask) noexcept
+            {
+                info.multisampler_info.pSampleMask = mask;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::add_multisampler_alpha_to_coverage(const VkBool32 coverage) noexcept
+            {
+                info.multisampler_info.alphaToCoverageEnable = coverage;
+
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::add_multisampler_alpha_to_one(const VkBool32 alpha) noexcept
+            {
+                info.multisampler_info.alphaToOneEnable = alpha;
+
+                return *this;
+            }
+
+            pipeline::builder& pipeline::builder::set_multisampler_pnext(const void* p_next) noexcept
+            {
+                return *this;
+            }
+
+            pipeline::builder& pipeline::builder::add_pipeline_layout_push_constant(const VkPushConstantRange push_constant) noexcept
+            {
+                info.push_constants.push_back(push_constant);
+                info.pipeline_layout_info.pushConstantRangeCount = static_cast<u32>(info.push_constants.size());
+                info.pipeline_layout_info.pPushConstantRanges = info.push_constants.data();
+                
+                return *this;
+            }
+            
+            pipeline::builder& pipeline::builder::add_pipeline_layout_descriptor_set(const VkDescriptorSetLayout descriptor_set) noexcept
+            {
+                info.descriptor_sets.push_back(descriptor_set);
+                info.pipeline_layout_info.setLayoutCount = static_cast<u32>(info.descriptor_sets.size());
+                info.pipeline_layout_info.pSetLayouts = info.descriptor_sets.data();
+
+                return *this;
+            }
+
+            pipeline::builder& pipeline::builder::set_pipeline_layout_pnext(const void* p_next) noexcept
+            {
+                info.pipeline_layout_info.pNext = p_next;
+                
+                return *this;
+            }
+
+            pipeline::builder& pipeline::builder::set_pipeline_layout_flags(const VkPipelineLayoutCreateFlags flags) noexcept
+            {
+                info.pipeline_layout_info.flags |= flags;
+
+                return *this;
+            }
+
             void pipeline::destroy() noexcept
             {
                 vkDestroyPipeline(_device.lock()->handle(), _pipeline, _allocation_callbacks);
--- include/blade/gfx/vulkan/pipeline.h
+++ include/blade/gfx/vulkan/pipeline.h
@@ -31,7 +31,7 @@ namespace blade
                                 : info { device }
                             {}
 
-                            std::optional<std::shared_ptr<pipeline>> build() const noexcept;
+                            std::optional<std::shared_ptr<pipeline>> build() noexcept;
 
                             builder& add_dynamic_state(const VkDynamicState dynamic_state) noexcept;
                             builder& set_type(const enum type type) noexcept;
@@ -45,6 +45,38 @@ namespace blade
                             builder& add_viewport(const VkViewport viewport) noexcept;
                             builder& add_scissor(const VkRect2D scissor) noexcept;
                             builder& set_extent(VkExtent2D extent) noexcept;
+
+                            builder& add_vertex_input_binding_description(const VkVertexInputBindingDescription) noexcept;
+                            builder& add_vertex_input_attribute_description(const VkVertexInputAttributeDescription) noexcept;
+
+                            builder& set_input_assembly_topology(const VkPrimitiveTopology) noexcept;
+                            builder& set_input_assembly_primitive_restart(const VkBool32) noexcept;
+                            builder& set_input_assembly_flags(const VkPipelineInputAssemblyStateCreateFlags) noexcept;
+                            builder& set_input_assembly_pnext(const void*) noexcept;
+
+                            builder& set_rasterization_depth_clamp(const VkBool32) noexcept; 
+                            builder& set_rasterization_discard_enable(const VkBool32) noexcept; 
+                            builder& set_rasterization_cull_mode(const VkCullModeFlags) noexcept;
+                            builder& set_rasterization_front_face(const VkFrontFace) noexcept;
+                            builder& set_rasterization_depth_bias(const VkBool32) noexcept;
+                            builder& set_rasterization_depth_bias_constant_factor(const f32) noexcept;
+                            builder& set_rasterization_depth_bias_clamp(const f32) noexcept;
+                            builder& set_rasterization_depth_bias_slope_factor(const f32) noexcept;
+                            builder& set_rasterization_line_width(const f32) noexcept;
+                            builder& set_rasterization_pnext(const void*) noexcept;
+
+                            builder& set_multisampler_rasterization_samples(const VkSampleCountFlagBits) noexcept;
+                            builder& set_multisampler_sample_shading(const VkBool32) noexcept;
+                            builder& add_multisampler_sample_mask(const VkSampleMask*) noexcept;
+                            builder& add_multisampler_alpha_to_coverage(const VkBool32) noexcept;
+                            builder& add_multisampler_alpha_to_one(const VkBool32) noexcept;
+                            builder& set_multisampler_pnext(const void*) noexcept;
+
+                            // TODO: make references?
+                            builder& add_pipeline_layout_descriptor_set(const VkDescriptorSetLayout) noexcept;
+                            builder& add_pipeline_layout_push_constant(const VkPushConstantRange) noexcept;
+                            builder& set_pipeline_layout_pnext(const void*) noexcept;
+                            builder& set_pipeline_layout_flags(const VkPipelineLayoutCreateFlags) noexcept;
                             
                             struct
                             {
@@ -60,49 +92,93 @@ namespace blade
                                 VkShaderModule fragment_shader_module                      { VK_NULL_HANDLE };
                                 enum type type                                             { type::graphics };
                                 VkRenderPass renderpass                                    { VK_NULL_HANDLE };
+                                
+                                std::vector<VkVertexInputBindingDescription> vertex_binding_descriptions     {};
+                                std::vector<VkVertexInputAttributeDescription> vertex_attribute_descriptions {};
 
+                                std::vector<VkDescriptorSetLayout> descriptor_sets {};
+                                std::vector<VkPushConstantRange> push_constants    {};
 
-                                // TODO: Add methods for enabling these
-                                struct
+                                VkPipelineVertexInputStateCreateInfo vertex_info           
+                                { 
+                                    .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+                                    .vertexBindingDescriptionCount = 0,
+                                    .pVertexBindingDescriptions = nullptr,
+                                    .vertexAttributeDescriptionCount = 0,
+                                    .pVertexAttributeDescriptions = nullptr
+                                };
+                                
+                                VkPipelineInputAssemblyStateCreateInfo input_assembly_info
+                                { 
+                                    .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+                                    .topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+                                    .primitiveRestartEnable = VK_FALSE,
+                                };
+                                
+                                VkPipelineViewportStateCreateInfo viewport_info            
+                                { 
+                                    .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+                                };
+                                
+                                VkPipelineRasterizationStateCreateInfo rasterization_info  
+                                { 
+                                    .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+                                    .depthClampEnable = VK_FALSE,
+                                    .rasterizerDiscardEnable = VK_FALSE,
+                                    .polygonMode = VK_POLYGON_MODE_FILL,
+                                    .cullMode = VK_CULL_MODE_BACK_BIT,
+                                    .frontFace = VK_FRONT_FACE_CLOCKWISE,
+                                    .depthBiasEnable = VK_FALSE,
+                                    .depthBiasConstantFactor = 0.0f,
+                                    .depthBiasClamp = 0.0f,
+                                    .depthBiasSlopeFactor = 0.0f,
+                                    .lineWidth = 1.0f,
+                                };
+                                
+                                VkPipelineMultisampleStateCreateInfo multisampler_info     
+                                { 
+                                    .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+                                    .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+                                    .sampleShadingEnable = VK_FALSE,
+                                    .minSampleShading = 1.0f,
+                                    .pSampleMask = nullptr,
+                                    .alphaToCoverageEnable = VK_FALSE,
+                                    .alphaToOneEnable = VK_FALSE,
+                                };
+                                
+                                VkPipelineLayoutCreateInfo pipeline_layout_info            
                                 {
-                                    VkPolygonMode polygon_mode { VK_POLYGON_MODE_FILL };
-                                    VkCullModeFlags cull_mode { VK_CULL_MODE_BACK_BIT };
-                                    VkFrontFace front_face { VK_FRONT_FACE_CLOCKWISE };
-                                    VkBool32 depth_bias_enable { VK_FALSE };
-                                    f32 depth_bias_constant_factor { 0.0f };
-                                    f32 depth_bias_clamp { 0.0f };
-                                    f32 depth_bias_slope_factor { 0.0f };
-                                    f32 line_width { 1.0f };
-                                } rasterization {};
-
-                                struct
+                                    .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+                                    .setLayoutCount = 0,
+                                    .pSetLayouts = nullptr,
+                                    .pushConstantRangeCount = 0,
+                                    .pPushConstantRanges = nullptr,
+                                };
+                               
+                                // TODO: better implementation of color blending
+                                VkPipelineColorBlendAttachmentState color_blend_attachment 
                                 {
-                                    VkSampleCountFlags sample_count   { VK_SAMPLE_COUNT_1_BIT };
-                                    VkBool32 sample_shading_enable    { VK_FALSE };
-                                    f32 min_sample_shading            { 1.0f };
-                                    VkSampleMask* sample_mask         { nullptr };
-                                    VkBool32 alpha_to_coverage_enable { VK_FALSE };
-                                    VkBool32 alpha_to_one_enable      { VK_FALSE };
-                                } multisampling {};
-
-                                struct
-                                {
-                                    VkBool32 enable { VK_FALSE };
-                                    VkBlendFactor color_src_factor { VK_BLEND_FACTOR_SRC_ALPHA };
-                                    VkBlendFactor color_dst_factor { VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA };
-                                    VkBlendOp color_op { VK_BLEND_OP_ADD };
-                                    VkBlendFactor alpha_src_factor { VK_BLEND_FACTOR_ONE };
-                                    VkBlendFactor alpha_dst_factor { VK_BLEND_FACTOR_ZERO };
-                                    VkBlendOp alpha_op { VK_BLEND_OP_ADD };
-                                    VkColorComponentFlags write_mask {
-                                        VK_COLOR_COMPONENT_R_BIT
+                                    .blendEnable = VK_FALSE,
+                                    .srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+                                    .dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+                                    .colorBlendOp = VK_BLEND_OP_ADD,
+                                    .srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE,
+                                    .dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+                                    .alphaBlendOp = VK_BLEND_OP_ADD,
+                                    .colorWriteMask = VK_COLOR_COMPONENT_R_BIT
                                         | VK_COLOR_COMPONENT_G_BIT
                                         | VK_COLOR_COMPONENT_B_BIT
-                                        | VK_COLOR_COMPONENT_A_BIT
-                                    };
-                                    VkLogicOp logic_op { VK_LOGIC_OP_COPY };
-                                    std::array<f32, 4> blend_constants { 0.0f, 0.0f, 0.0f, 0.0f };
-                                } color_blending {};
+                                        | VK_COLOR_COMPONENT_A_BIT,
+                                };
+                                VkPipelineColorBlendStateCreateInfo color_blend_info       
+                                { 
+                                    .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+                                    .logicOpEnable = VK_FALSE,
+                                    .logicOp = VK_LOGIC_OP_COPY,
+                                    .attachmentCount = 1,
+                                    .pAttachments = &color_blend_attachment,
+                                    .blendConstants = { 0.0f, 0.0f, 0.0f, 0.0f },
+                                };
 
                             } info {};
                     };
